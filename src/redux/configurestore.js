230672470b4441fc754d8a4c923e6b6ef844a657
Caleb Anderson <buyananderson@gmail.com>
2021-09-06 16:28:42 +0200

    Configure the store

--- src/redux/configurestore.js
+++ src/redux/configurestore.js
@@ -1,2 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+import rocketsReducer from './rockets/rockets';
+
+const reducer = combineReducers({
+  rocketsReducer,
+});
+
+const store = configureStore({
+  reducer,
+});
+
+export default store;
