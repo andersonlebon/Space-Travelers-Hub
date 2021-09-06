c124550c697facdbe05965ed76c637efc78e3424
Caleb Anderson <buyananderson@gmail.com>
2021-09-06 15:02:11 +0200

    Add components and routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/header';
+import Mission from './components/mission';
+import Profile from './components/profile';
+import Rockets from './components/rockets';
 import './sass/App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main className="App">
+      <Header />
+      <BrowserRouter>
+        <Route path="/" component={Rockets} />
+        <Route path="/mission" component={Mission} />
+        <Route path="/profile" component={Profile} />
+      </BrowserRouter>
+    </main>
+  );
 }
 
 export default App;
